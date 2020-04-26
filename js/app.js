37d612cdeb811d104618988537e4954a728a2f3b
mo-alaa88 <mohammed.alaa.al.din@gmail.com>
2020-04-27 00:54:48 +0200

    add admin.html & AppAd.js

--- js/app.js
+++ js/app.js
@@ -1,41 +1,13 @@
-const countEl = document.getElementById('count'),
-     plus = document.getElementById('plus'),
-	 minus = document.getElementById('minus'),
-	 updateCount =  document.getElementById('updateCount');
-plus.addEventListener('click',()=>{
-	incrementVisitCount()
-})
+const countEl = document.getElementById('count');
 currentNum()
 
-//updateVisitCount();
-minus.addEventListener('click',()=>{
-	decrementVisitCount()
-})
-function incrementVisitCount() {
-	// fetch('https://api.countapi.xyz/update/florin-popcom/codepen/?amount=1')
-	fetch('https://api.countapi.xyz/update/youtube.com/?amount=1')
-	.then(res => res.json())
-	.then(res => {
-		countEl.innerHTML = res.value;
-	})
-	currentNum()
-}
-
-function decrementVisitCount() {
-	// fetch('https://api.countapi.xyz/update/florin-popcom/codepen/?amount=-1')
-	fetch('https://api.countapi.xyz/update/youtube.com/?amount=-1')
-	.then(res => res.json())
-	.then(res => {
-		countEl.innerHTML = res.value;
-	})
-}
 // https://api.countapi.xyz/hit/youtube.com/
 
 function currentNum (){
 	fetch(`https://api.countapi.xyz/get/youtube.com/`)
 	.then(res=>res.json())
 	.then(res => {
-		updateCount.innerHTML = res.value-1;
+		countEl.innerHTML = res.value;
 	})
 }
 
